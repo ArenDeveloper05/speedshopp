f849e8b32000de04358b3d15fb2716d7664effe8
ArenDeveloper05 <antisystem005@mail.ru>
2023-11-07 17:28:00 +0400

    gitignore

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -12,83 +12,63 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-categories info">
           <div className="title">
-              {
-                infoData.map(({title}) => {
-                  return (
-                    <p className="category-title">{title}</p>
-                  )
-                })
-              }
+            {infoData.map(({ title }) => {
+              return <p className="category-title">{title}</p>;
+            })}
           </div>
-              <div className="category">
-                {
-                  infoData.map(({name}) => {
-                    return (
-                      <a href="/" className="category-text">{name}</a>
-                    )
-                  })
-                }
-              </div>
+          <div className="category">
+            {infoData.map(({ name }) => {
+              return (
+                <a href="/" className="category-text">
+                  {name}
+                </a>
+              );
+            })}
+          </div>
         </div>
         <div className="footer-categories my-page">
-        <div className="title">
-              {
-               myPageData.map(({title}) => {
-                  return (
-                    <p className="category-title">{title}</p>
-                  )
-                })
-              }
+          <div className="title">
+            {myPageData.map(({ title }) => {
+              return <p className="category-title">{title}</p>;
+            })}
           </div>
           <div className="category">
-                {
-                  myPageData.map(({name}) => {
-                    return (
-                      <a href="/" className="category-text">{name}</a>
-                    )
-                  })
-                }
-              </div>
+            {myPageData.map(({ name }) => {
+              return (
+                <a href="/" className="category-text">
+                  {name}
+                </a>
+              );
+            })}
+          </div>
         </div>
         <div className="footer-categories product-tags">
-        <div className="title">
-              {
-               productTagsData.map(({title}) => {
-                  return (
-                    <p className="category-title">{title}</p>
-                  )
-                })
-              }
+          <div className="title">
+            {productTagsData.map(({ title }) => {
+              return <p className="category-title">{title}</p>;
+            })}
           </div>
           <div className="category-tags">
-                {
-                  productTagsData.map(({name}) => {
-                    return (
-                      <button className="category-button">{name}</button>
-                    )
-                  })
-                }
-              </div>
+            {productTagsData.map(({ name }) => {
+              return <button className="category-button">{name}</button>;
+            })}
+          </div>
         </div>
         <div className="footer-categories follow-us">
-        <div className="title">
-              {
-               followUs.map(({title}) => {
-                  return (
-                    <p className="category-title">{title}</p>
-                  )
-                })
-              }
+          <div className="title">
+            {followUs.map(({ title }) => {
+              return <p className="category-title">{title}</p>;
+            })}
           </div>
           <div className="category">
-                {
-                  followUs.map(({name}) => {
-                    return (
-                        <a href="/" className="category-text">{name}</a>
-                    )
-                  })
-                }
-              </div>
+            {followUs.map(({ name }) => {
+              return (
+                <a href="/" className="category-text">
+                  {name}
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
     </footer>
