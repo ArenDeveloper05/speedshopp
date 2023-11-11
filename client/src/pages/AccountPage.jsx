496888ff9c6208dfa087ed8a25b871225fde0dde
muradiian <muradyannorayr6@gmail.com>
2023-11-12 00:25:28 +0400

    accountPage

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,10 @@
+import Layout from '../layout/Layout'
+import AccountAbout from '../components/account-about/AccountAbout'
+
+function AccountPage() {
+    return(
+        <AccountAbout/>
+    )
+}
+
+export default AccountPage
