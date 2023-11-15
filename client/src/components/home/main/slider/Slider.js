2d14bb208ca902ae23a1a4323c62a177c4e85f23
Razmik Simonyan <razyan.moretto@gmail.com>
2023-11-15 07:51:58 -0800

    main-page slider

--- client/src/components/home/main/slider/Slider.js
+++ client/src/components/home/main/slider/Slider.js
@@ -16,72 +16,59 @@ import {Autoplay, EffectCoverflow, Navigation, Pagination} from 'swiper/modules'
 export default function Slider() {
     return (
         <div className={'slider'}>
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                spaceBetween={0}
-                slidesPerView={1.2}
-                navigation={true}
-                autoplay={{
-                    delay: 6000,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 50,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-                modules={[Navigation, EffectCoverflow, Pagination, Autoplay]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <img src="/images/phenixImg.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className={'slider_content'}>
-                        <div className={'slider_content_text_area'}>
-                            <p>
-                                Ստեղծեք ձեր ինտերնետ խանութը
-                                մեր կայքում եվ սկսեք
-                                վաճառել մեզ հետ
-                            </p>
-                        </div>
-                        <a href={'/'}>
-                            <button className={'slider_content_btn'}>
-                                Իմանալ ավելին
-                                <div className={'slider_btn_icon'}></div>
-                            </button>
-                        </a>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/fastex.png" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/idram.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/idram.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/parma.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/phenixImg.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/phenixImg.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/images/phenixImg.jpg" />
-                </SwiperSlide>
-            </Swiper>
+            <div className={'slider_blure'}></div>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    spaceBetween={0}
+                    slidesPerView={1.2}
+                    navigation={true}
+                    autoplay={{
+                        delay: 6000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 50,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    modules={[Navigation, EffectCoverflow, Pagination, Autoplay]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src="/images/phenixImg.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/phenixImg.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/fastex.png" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/idram.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/idram.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/parma.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/phenixImg.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/phenixImg.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/images/phenixImg.jpg" />
+                    </SwiperSlide>
+                </Swiper>
         </div>
     );
 }
